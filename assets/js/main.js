a1384980c38256ad601a3fea130de2712b2dc63e
capslockintro <141958078+capslockintro@users.noreply.github.com>
2023-08-11 13:13:23 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector(".preloader-content_button");
 let animInterval;
 let loadInterval;
 
-const videoId = "jToKZAWTNyQ"
+const videoId = "3qFUjcoHTsg"
 YT.ready(_=>{
     window.player = new YT.Player("player", {
         videoId: videoId,
