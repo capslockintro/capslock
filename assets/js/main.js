6694ddcf02ac691a5345a9e3a3e5961d85779003
capslockintro <141958078+capslockintro@users.noreply.github.com>
2023-08-11 13:15:14 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ YT.ready(_=>{
             "onReady": e => {
                 e.target.setVolume(50);
                 clearInterval(loadInterval)                    
-                button.innerText = "Welcome to m1gwan profile"
+                button.innerText = "Dont be afraid, just click"
                 button.classList.add("ready");
                 button.addEventListener("click", () => {
                     animInterval = setInterval(preloaderAnim, 10);
